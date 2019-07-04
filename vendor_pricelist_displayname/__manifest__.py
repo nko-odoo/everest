0c780625c6a21e3b19c1a7bb983f27f794798b35
Nicholas Kosinski <52519549+nko-odoo@users.noreply.github.com>
2019-07-03 22:13:39 -0700

    Add files via upload

---
+++ vendor_pricelist_displayname/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Vendor Pricelist Display Name',
+    'version': '1.0.0',
+    'summary': 'Display Alternate SKU instead of Vendor',
+    'depends': ['base'],
+    'data': [
+    ],
+}
