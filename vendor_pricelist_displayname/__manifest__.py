2d54b801b950e9510aff09d0f7e8c79adf1c131d
Nicholas Kosinski <52519549+nko-odoo@users.noreply.github.com>
2019-07-03 22:17:28 -0700

    Update __manifest__.py

--- vendor_pricelist_displayname/__manifest__.py
+++ vendor_pricelist_displayname/__manifest__.py
@@ -3,7 +3,7 @@
     'name': 'Vendor Pricelist Display Name',
     'version': '1.0.0',
     'summary': 'Display Alternate SKU instead of Vendor',
-    'depends': ['base'],
+    'depends': ['base','product','purchase'],
     'data': [
     ],
 }
